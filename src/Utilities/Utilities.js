c7a44f0486d9440794070ddc59127477628c9507
kainklob <klobk23@gmail.com>
2022-09-27 15:57:28 -0400

    added base Utilities class + redux / thunk packages

---
+++ src/Utilities/Utilities.js
@@ -0,0 +1,9 @@
+class Utilities {
+
+    getData(chamber) {
+        // Returns Object to be placed in Redux State - Details in "Redux State Structure" of readme.md
+        return `Getting ${chamber}`;
+    }
+}
+
+export default Utilities;
