2f4dbcad6ad294a117539e396668b49a638ea79f
kainklob <klobk23@gmail.com>
2022-09-27 19:10:12 -0400

    updated package.json + configured test store

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 function App() {
+
+    const { test } = { test: useSelector(state => state.test) }
 
     return (
         <div id="app">
             <p>This is the app</p>
+            <p>{test}</p>
         </div>
     )
 }
---
+++ src/app/rootReducer.js
@@ -0,0 +1,7 @@
+const INI_STATE = { test: "This is a test" }
+
+function rootReducer(state = INI_STATE, action) {
+    return state;
+}
+
+export default rootReducer;
