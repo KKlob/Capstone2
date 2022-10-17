b087263a9c0188d1e11dc74534bb7301d1a022bb
kainklob <klobk23@gmail.com>
2022-10-17 12:53:31 -0400

    added comments to clarify necessary additions to express backend

--- server/index.js
+++ server/index.js
@@ -7,9 +7,18 @@ const app = express();
 
 app.use(express.static(path.resolve(__dirname, '../Client/build')));
 
-// Logic for SQL DB 
+// Logic for Base API calls to gather all necessary Raw info + Logic for Sanitation of data
+
+// Logic for SQL DB
+// Initializing info post-api calls and sanitation. Update if info already exists in db 
 
 // API ROUTES
+
+/** Routes include: 
+ *      - /api/congress | returns obj for Redux State
+ *      - /api/login | /api/loggout | /api/createUser - handle user creation/Login/Loggout
+*/
+
 app.get("/api", (req, res) => {
     res.json({ message: "Hello from server!" });
 });
